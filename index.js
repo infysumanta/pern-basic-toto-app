3444ec252999f8f89785927234dc151fb3f7f3c3
Sumanta Kabiraj <sumantablog@gmail.com>
2023-03-25 09:07:24 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-
-const port = process.env.PORT || 3000;
+const config = require("./config")
+const port = config.PORT;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
