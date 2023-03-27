9ba006807f004c208786f0092c12cde83b3664bd
Sumanta Kabiraj <sumantablog@gmail.com>
2023-03-27 09:09:21 +0000

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan")
 const cors = require("cors")
 app.use(express.json());
 app.use(cors());
+app.use(logger("dev"));
 
 module.exports = app;
